4511a4893665cdcf262b7d1fe651881de47a7ca6
ogapy <kyon6690@gmail.com>
2023-09-13 23:59:57 +0900

    fix

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-1_MT04Sfcmm', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '43rbku77gevsmn1kq0dred91sr
-', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-1_MT04Sfcmm',
+        userPoolClientId: '43rbku77gevsmn1kq0dred91sr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
